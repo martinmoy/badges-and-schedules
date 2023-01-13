6122e4f9852fbaf2f3cfde7bcbd56799e2ee01ae
Martin Moy <martinmoy1991@gmail.com>
2023-01-12 23:29:24 -0500

    commit tree c786e9214100fbd9203351e1fbff0f12d9cc1670

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect do |name| 
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(speaker)
+  speaker.each_with_index.collect do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(array)
+     batch_badge_creator(array).each do |name|
+      puts name
+     end
+     assign_rooms(array).each do |rooms|
+      puts rooms
+     end
+  
+end
